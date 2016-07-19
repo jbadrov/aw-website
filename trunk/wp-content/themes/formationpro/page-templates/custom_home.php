b8554bcf404c7ab2001f65c325e5675a606cf7fb
jbadrov <jbadrov@2f5d300a-12b5-43ce-915b-723bbad5b47f>
2016-07-19 02:48:11 +0000

    changed to slider_description for name of field

--- trunk/wp-content/themes/formationpro/page-templates/custom_home.php
+++ trunk/wp-content/themes/formationpro/page-templates/custom_home.php
@@ -14,9 +14,9 @@ get_header(); ?>
         <?php
 
           $featured_cat   =   get_theme_mod( 'homepage_slider_cat' );
-		  
+
           $number         =   get_theme_mod( 'homepage_slider_slide_no' );
-		  
+
 		  $slider_arg = array(
 			  	'post_type'=>array('post','page'),
                 'showposts' => $number,
@@ -24,9 +24,9 @@ get_header(); ?>
               );
 
           $the_query     =   new WP_Query($slider_arg);
-          
+
         ?>
-            
+
         <div class="flex-container">
           <div class="flexslider">
             <ul class="slides">
@@ -38,7 +38,7 @@ get_header(); ?>
                       <div class="flex-caption-title">
                         <a href="<?php the_permalink() ?>"><?php the_title(); ?></a>
                       </div>
-                      <p><?php $slide_desc = get_post_meta(get_the_ID(),'description',true); echo ($slide_desc)?substr(trim($slide_desc),0,250):esc_html(get_slider_excerpt()); ?> <a href="<?php the_permalink() ?>">...</a></p>
+                      <p><?php $slide_desc = get_post_meta(get_the_ID(),'slider_description',true); echo ($slide_desc)?substr(trim($slide_desc),0,250):esc_html(get_slider_excerpt()); ?> <a href="<?php the_permalink() ?>"></a></p>
                     </div>
                   </div>
                 </li>
@@ -49,10 +49,10 @@ get_header(); ?>
 
       <?php endif; ?>
 
-    
 
 
-      
+
+
       <?php if(! get_theme_mod('homepage_service_bool')): ?>
 
         <div class="section_services group">
@@ -60,7 +60,7 @@ get_header(); ?>
 
              <?php if (class_exists('FormationPlus')) : ?>
 
-              <div class="service-box">        
+              <div class="service-box">
                 <div class="featuretext">
                   <?php if ( get_theme_mod( 'header-one-file-upload' ) ) : ?>
                     <a href="<?php echo esc_url( get_theme_mod( 'header_one_url' ) ); ?>"><img src="<?php echo esc_url( get_theme_mod( 'header-one-file-upload' ) ); ?>"  alt="feature one"></a>
@@ -72,7 +72,7 @@ get_header(); ?>
                 </div>
               </div>
 
-              <div class="service-box">         
+              <div class="service-box">
                 <div class="featuretext">
                 <?php if ( get_theme_mod( 'header-two-file-upload' ) ) : ?>
                   <a href="<?php echo esc_url( get_theme_mod( 'header_two_url' ) ); ?>"><img src="<?php echo esc_url( get_theme_mod( 'header-two-file-upload' ) ); ?>"  alt="feature two"></a>
@@ -84,7 +84,7 @@ get_header(); ?>
                 </div>
               </div>
 
-              <div class="service-box">         
+              <div class="service-box">
                 <div class="featuretext">
                 <?php if ( get_theme_mod( 'header-three-file-upload' ) ) : ?>
                   <a href="<?php echo esc_url( get_theme_mod( 'header_three_url' ) ); ?>"><img src="<?php echo esc_url( get_theme_mod( 'header-three-file-upload' ) ); ?>"  alt="feature three"></a>
@@ -95,8 +95,8 @@ get_header(); ?>
                 <p><?php echo esc_html(get_theme_mod( 'featured_textbox_text_three' ) ); ?></p>
                 </div>
               </div>
-              
-              <div class="service-box">         
+
+              <div class="service-box">
                 <div class="featuretext">
                 <?php if ( get_theme_mod( 'header-four-file-upload' ) ) : ?>
                   <a href="<?php echo esc_url( get_theme_mod( 'header_four_url' ) ); ?>"><img src="<?php echo esc_url( get_theme_mod( 'header-four-file-upload' ) ); ?>"  alt="feature four"></a>
@@ -107,8 +107,8 @@ get_header(); ?>
                 <p><?php echo esc_html(get_theme_mod( 'featured_textbox_text_four' ) ); ?></p>
                 </div>
               </div>
-              
-              <div class="service-box">         
+
+              <div class="service-box">
                 <div class="featuretext">
                 <?php if ( get_theme_mod( 'header-five-file-upload' ) ) : ?>
                   <a href="<?php echo esc_url( get_theme_mod( 'header_five_url' ) ); ?>"><img src="<?php echo esc_url( get_theme_mod( 'header-five-file-upload' ) ); ?>"  alt="feature five"></a>
@@ -119,8 +119,8 @@ get_header(); ?>
                 <p><?php echo esc_html(get_theme_mod( 'featured_textbox_text_five' ) ); ?></p>
                 </div>
               </div>
-              
-              <div class="service-box">         
+
+              <div class="service-box">
                 <div class="featuretext">
                 <?php if ( get_theme_mod( 'header-six-file-upload' ) ) : ?>
                   <a href="<?php echo esc_url( get_theme_mod( 'header_six_url' ) ); ?>"><img src="<?php echo esc_url( get_theme_mod( 'header-six-file-upload' ) ); ?>"  alt="feature six"></a>
@@ -131,8 +131,8 @@ get_header(); ?>
                 <p><?php echo esc_html(get_theme_mod( 'featured_textbox_text_six' ) ); ?></p>
                 </div>
               </div>
-              
-              <div class="service-box">         
+
+              <div class="service-box">
                 <div class="featuretext">
                 <?php if ( get_theme_mod( 'header-seven-file-upload' ) ) : ?>
                   <a href="<?php echo esc_url( get_theme_mod( 'header_seven_url' ) ); ?>"><img src="<?php echo esc_url( get_theme_mod( 'header-seven-file-upload' ) ); ?>"  alt="feature seven"></a>
@@ -143,8 +143,8 @@ get_header(); ?>
                 <p><?php echo esc_html(get_theme_mod( 'featured_textbox_text_seven' ) ); ?></p>
                 </div>
               </div>
-              
-              <div class="service-box">         
+
+              <div class="service-box">
                 <div class="featuretext">
                 <?php if ( get_theme_mod( 'header-eight-file-upload' ) ) : ?>
                   <a href="<?php echo esc_url( get_theme_mod( 'header_eight_url' ) ); ?>"><img src="<?php echo esc_url( get_theme_mod( 'header-eight-file-upload' ) ); ?>"  alt="feature eight"></a>
@@ -155,8 +155,8 @@ get_header(); ?>
                 <p><?php echo esc_html(get_theme_mod( 'featured_textbox_text_eight' ) ); ?></p>
                 </div>
               </div>
-              
-              <div class="service-box">         
+
+              <div class="service-box">
                 <div class="featuretext">
                 <?php if ( get_theme_mod( 'header-nine-file-upload' ) ) : ?>
                   <a href="<?php echo esc_url( get_theme_mod( 'header_nine_url' ) ); ?>"><img src="<?php echo esc_url( get_theme_mod( 'header-nine-file-upload' ) ); ?>"  alt="feature nine"></a>
@@ -173,7 +173,7 @@ get_header(); ?>
 
             <div class="section group">
 
-              <div class="col span_1_of_3 box-1">        
+              <div class="col span_1_of_3 box-1">
                 <div class="featuretext">
                   <?php if ( get_theme_mod( 'header-one-file-upload' ) ) : ?>
                     <a href="<?php echo esc_url( get_theme_mod( 'header_one_url' ) ); ?>"><img src="<?php echo esc_url( get_theme_mod( 'header-one-file-upload' ) ); ?>"  alt="feature one"></a>
@@ -185,7 +185,7 @@ get_header(); ?>
                 </div>
               </div>
 
-              <div class="col span_1_of_3 box-2">         
+              <div class="col span_1_of_3 box-2">
                 <div class="featuretext">
                 <?php if ( get_theme_mod( 'header-two-file-upload' ) ) : ?>
                   <a href="<?php echo esc_url( get_theme_mod( 'header_two_url' ) ); ?>"><img src="<?php echo esc_url( get_theme_mod( 'header-two-file-upload' ) ); ?>"  alt="feature two"></a>
@@ -197,7 +197,7 @@ get_header(); ?>
                 </div>
               </div>
 
-              <div class="col span_1_of_3 box-3">         
+              <div class="col span_1_of_3 box-3">
                 <div class="featuretext">
                 <?php if ( get_theme_mod( 'header-three-file-upload' ) ) : ?>
                   <a href="<?php echo esc_url( get_theme_mod( 'header_three_url' ) ); ?>"><img src="<?php echo esc_url( get_theme_mod( 'header-three-file-upload' ) ); ?>"  alt="feature three"></a>
@@ -213,8 +213,8 @@ get_header(); ?>
 
           <?php endif; ?>
 
-        </div> 
-      </div> 
+        </div>
+      </div>
 
     <?php endif; ?>
 
@@ -227,12 +227,12 @@ get_header(); ?>
       <div class="featuretext_top">
 
         <h3 class="animated" data-fx="slideInLeft"><?php if( get_theme_mod( 'featured_textbox' ) ){ echo esc_html(get_theme_mod( 'featured_textbox' ) ); } else { _e( 'Promotional Bar', 'formationpro' ); } ?></h3>
-        
+
         <?php if(get_theme_mod( 'featured_button_txt' )): ?>
 
           <div class="featuretext_button animated" data-fx="slideInRight">
             <a href="<?php echo esc_url( get_theme_mod( 'featured_button_url' ) ); ?>"><?php echo esc_attr(get_theme_mod( 'featured_button_txt' )); ?></a>
-          </div>  
+          </div>
 
         <?php endif; ?>
 
@@ -245,7 +245,7 @@ get_header(); ?>
 
       <div id="content" class="fullwidth" role="main">
 
-      
+
         <?php if(! get_theme_mod('homepage_recent_bool')): ?>
 
           <div class="section_thumbnails group animated" data-fx="fadeInUp">
@@ -270,20 +270,20 @@ get_header(); ?>
                         echo '<img alt="post" class="imagerct" src="' . $image_src[0] . '">';
                       }
                     ?>
-                    <div class="mask">  
-                      <a href="<?php the_permalink(); ?>" class="info">Full Image</a>  
+                    <div class="mask">
+                      <a href="<?php the_permalink(); ?>" class="info">Full Image</a>
                     </div>
                   </div>
                   <!-- <div class="recent_title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></div> -->
-                  <p><?php 
+                  <p><?php
 				  $post_desc = get_post_meta(get_the_ID(),'description',true);
-				  echo ($post_desc)?substr(trim($post_desc),0,250):formationpro_get_recentposts_excerpt(); 
+				  echo ($post_desc)?substr(trim($post_desc),0,250):formationpro_get_recentposts_excerpt();
 				 	?></p>
                   <div class="thumbs-more-link">
                     <a href="<?php the_permalink() ?>"> <?php $call2action=get_post_meta($post->ID,'call2action',true);if(!$call2action)$call2action='Read More';  echo __($call2action, 'formationpro'); ?></a>
                   </div>
                 </article>
-              </div>	
+              </div>
 
             <?php endwhile; ?>
 
@@ -298,15 +298,15 @@ get_header(); ?>
           <div class="testimonial_home animated" data-fx="fadeInUp">
 
             <?php if(! get_theme_mod('homepage_testimonials_title_bool')): ?>
-              <h3><?php 
+              <h3><?php
                 if( get_theme_mod('testimonials_title') ){
                   echo esc_attr( get_theme_mod('testimonials_title') );
                 } else {
                   _e( 'Customer Testimonial', 'formationpro' );
-                } 
+                }
               ?></h3>
             <?php endif; ?>
-            
+
             <?php echo do_shortcode('[testimonial posts_per_page="1" orderby="none" testimonial_id="' . get_theme_mod('testimonials_id') . '"]'); ?>
 
           </div>
@@ -315,7 +315,7 @@ get_header(); ?>
 
 
 
-        <?php if(! get_theme_mod('homepage_partners_bool')): ?> 
+        <?php if(! get_theme_mod('homepage_partners_bool')): ?>
 
           <div class="section_clients group">
             <div class="client animated" data-fx="fadeInUp">
@@ -323,7 +323,7 @@ get_header(); ?>
               <h3><?php if(get_theme_mod('homepage_partners_title')){ echo esc_html( get_theme_mod( 'homepage_partners_title' ) ); } else { _e('Partners', 'formationpro'); } ?></h3>
 
               <div class="unity-separator"></div>
-              
+
               <div class="col span_1_of_4">
                 <div class="client_recent">
                   <?php if ( get_theme_mod( 'logo-one-file-upload' ) ) : ?>
