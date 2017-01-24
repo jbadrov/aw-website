3e54c671c62e67f13868a8c16eb0322dfe6b59cd
saeed <saeed@2f5d300a-12b5-43ce-915b-723bbad5b47f>
2017-01-24 12:34:33 +0000

    File upload section

---
+++ trunk/file_upload.php
@@ -0,0 +1,111 @@
+<?php
+
+    function my_custom_email_content_type( $content_type ) {
+        return 'text/html';
+    }
+		$targetfolder = "dropzone/files/".$_POST[ 'userEmail' ].'/';
+if( isset( $_POST[ 'submit' ] ) && isset( $_POST[ 'userEmail' ]) && !empty( $_POST[ 'userEmail' ])) {
+
+    $files = $_FILES[ 'file' ];
+    $upload_overrides = array( 'test_form' => false );
+
+    $attachments = array();
+		if (!file_exists($targetfolder)) {
+			mkdir($targetfolder, 0777, true);
+		}
+		move_uploaded_file($_FILES['file']['tmp_name'], $targetfolder.basename($_FILES['file']['name']));
+		foreach($_FILES['file']['tmp_name'] as $i=>$file){
+			move_uploaded_file($file, $targetfolder.basename($_FILES['file']['name'][$i]));
+		}
+		$attachments = array();
+		if (is_dir($targetfolder)){
+		  if ($dht = opendir($targetfolder)){
+			while (($file = readdir($dht)) !== false){
+				if($file=='..' || $file=='.'){
+					continue;
+				}
+				$attachments []= $targetfolder.$file;
+			}
+			closedir($dht);
+		  }
+		}		
+	$form_submission_id ='ID'. substr(number_format(time() * rand(),0,'',''),0,6);
+	$message = 'Hi '.$POST['Name'].'! Please find attachment sent on '.$POST['date_entered'].'';
+	 create_zip($attachments,$targetfolder.'my-archive.zip');
+	 $sent_response = mail_attachment('my-archive.zip', $targetfolder, $_POST[ 'userEmail' ], 'ht.test7@gmail.com', 'Helfertech', 'amir.khan@helfertech.com', 'Test Form Email', $message);
+	 echo $sent_response;die;
+} 
+function create_zip($files = array(),$destination = '',$overwrite = false) {
+	//if the zip file already exists and overwrite is false, return false
+	if(file_exists($destination) && !$overwrite) { return false; }
+	//vars
+	$valid_files = array();
+	//if files were passed in...
+	if(is_array($files)) {
+		//cycle through each file
+		foreach($files as $file) {
+			//make sure the file exists
+			if(file_exists($file)) {
+				$valid_files[] = $file;
+			}
+		}
+	}
+	//if we have good files...
+	if(count($valid_files)) {
+		//create the archive
+		$zip = new ZipArchive();
+		if($zip->open($destination,$overwrite ? ZIPARCHIVE::OVERWRITE : ZIPARCHIVE::CREATE) !== true) {
+			return false;
+		}
+		//add the files
+		foreach($valid_files as $file) {
+			$zip->addFile($file,$file);
+		}
+		//debug
+		//echo 'The zip archive contains ',$zip->numFiles,' files with a status of ',$zip->status;
+		
+		//close the zip -- done!
+		$zip->close();
+		
+		//check to make sure the file exists
+		return file_exists($destination);
+	}
+	else
+	{
+		return false;
+	}
+}
+function mail_attachment($filename, $path, $mailto, $from_mail, $from_name, $replyto, $subject, $message) {
+	
+	    $file = $path.$filename;
+	    $file_size = filesize($file);
+	    $handle = fopen($file, "r");
+	    $content = fread($handle, $file_size);
+	    fclose($handle);
+	    $content = chunk_split(base64_encode($content));
+	    
+	    $uid = md5(uniqid(time()));
+	    
+	    $header = $from_mail."\r\n";
+	    $header .= "Reply-To: ".$replyto."\r\n";
+	    $header .= "MIME-Version: 1.0\r\n";
+	    $header .= "Content-Type: multipart/mixed; boundary=\"".$uid."\"\r\n\r\n";
+	    $header .= "This is a multi-part message in MIME format.\r\n";
+	    $header .= "--".$uid."\r\n";
+	    $header .= "Content-type:text/plain; charset=iso-8859-1\r\n";
+	    $header .= "Content-Transfer-Encoding: 7bit\r\n\r\n";
+	    $header .= $message."\r\n\r\n";
+	    $header .= "--".$uid."\r\n";
+	    $header .= "Content-Type: application/octet-stream; name=\"".$filename."\"\r\n";
+	    $header .= "Content-Transfer-Encoding: base64\r\n";
+	    $header .= "Content-Disposition: attachment; filename=\"".$filename."\"\r\n\r\n";
+	    $header .= $content."\r\n\r\n";
+	    $header .= "--".$uid."--";
+	    // Messages for testing only, nobody will see them unless this script URL is visited manually
+	    if (mail($mailto, $subject, "", $header)) {
+	        return "Yes";
+	    } else {
+	        return "No";
+	    }
+	    
+	}
---
+++ trunk/wp-content/themes/formationpro/screenshots.php
@@ -0,0 +1,104 @@
+<?php
+
+/*
+Template Name: Screenshots
+
+*/
+add_action( 'wp_enqueue_scripts', function(){
+	wp_enqueue_style( 'style-name', get_template_directory_uri() . '/css/history.css' );
+});
+
+// if(count($_POST)>0) {
+	// print"<pre>"; print_r($_POST);
+// }
+get_header('centro'); ?>
+<script type="text/javascript" src="dropzone.min.js"></script>
+<link href="dropzone.min.css" rel="stylesheet" />
+ <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<div id="primary_home" class="content-area">
+	<div id="content" class="fullwidth" role="main">
+
+		<head>
+		</head>
+		<body>
+		<form name="screenshotForm"  id="screenshotForm" action="file_upload.php" method="post" enctype="multipart/form-data">
+		<table border="0" width="500" align="center" class="table">
+		<tr>
+		<td> Name</td>
+		<td><input type="text" class="InputBox" name="Name" value=""></td>
+		</tr>
+
+		<tr>
+		<td>Email</td>
+		<td><input type="text" class="InputBox" name="userEmail" value=""></td>
+		</tr>
+		<tr>
+		<td>Date</td>
+		<td><input type="date" name="date_entered"></td>
+		</tr>
+		</table>
+			<div id="dZUpload" class="dropzone">
+						 <div class="fallback">
+						  <input name="file[]" type="file" multiple />
+						 </div>
+						   <div class="dz-default dz-message">
+						  Drag files here to upload, or click to browse for files.
+						   </div>
+			</div>		
+		<div>
+		<input type="button" name="submit" id="submit" value="Submit" class="btnRegister">
+		</div>
+		</form>	
+	</div>
+		<div class="request-from-area">
+				<div class="user-info-area">
+					<div class="support-message"><br /><br />
+						<p>&nbsp;<br/>For support contact <a href="mailto:centro@emailautonomy.com">centro@emailautonomy.com</a></p><br /><br />
+
+					</div>
+				</div>
+		</div>
+</div>
+
+	</div><!-- #content .site-content -->
+</div><!-- #primary .content-area -->
+ <script type="text/javascript">
+// $(document).ready(function(){
+	// Dropzone.autoDiscover = false;
+	var element = "#dZUpload";
+	// console.log(element);
+	var myDropzone = new Dropzone(element,{
+		url: "file_upload.php",
+        addRemoveLinks: true,
+		uploadMultiple: true,
+		parallelUploads: 25,
+		maxFiles: 25,
+		autoProcessQueue: false,
+		init: function() {
+			dzClosure = this;
+			document.getElementById("submit").addEventListener("click", function(e) {
+					e.stopImmediatePropagation();
+					e.preventDefault();
+					e.stopPropagation();
+					dzClosure.processQueue();
+				// }
+				return false;
+			});
+
+			//send all the form data along with the files:
+			this.on("sendingmultiple", function(data, xhr, formData) {
+				//formData.append($('form').serializeArray());
+				var inputs = $('#screenshotForm :input');
+
+				var values = {};
+				inputs.each(function() {
+					var name = $(this).attr('name');
+					var val = $('[name="'+name+'"]').val();
+					formData.append(name, val);
+				});
+			});			
+		}
+     });
+    // });
+ </script>
+<?php get_footer('centro'); ?>
