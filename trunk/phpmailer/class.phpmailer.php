186f40e641cac4e7e0d651e067dbe72ff2511d1d
saeed <saeed@2f5d300a-12b5-43ce-915b-723bbad5b47f>
2017-01-25 16:16:35 +0000

    added smtp class reference

--- trunk/phpmailer/class.phpmailer.php
+++ trunk/phpmailer/class.phpmailer.php
@@ -25,6 +25,7 @@
  * @author Andy Prevost (codeworxtech) <codeworxtech@users.sourceforge.net>
  * @author Brent R. Matzelle (original founder)
  */
+ require_once('class.smtp.php');
 class PHPMailer
 {
     /**
