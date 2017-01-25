c605af58928c1151dadc63c74a144ae64ac84817
saeed <saeed@2f5d300a-12b5-43ce-915b-723bbad5b47f>
2017-01-25 17:35:03 +0000

    add email config

---
+++ phpmailer/config.php
@@ -0,0 +1,10 @@
+<?php
+$email_config = array(
+	'host' => 'smtp.gmail.com',
+	'user_name' => 'ht.test7@gmail.com',
+	'password' => '.ht237!!',
+	'smtp' => 'tls',
+	'port' => '587',
+	'from_email' => 'ht.test7@gmail.com',
+	'from_name' => 'AutonomyWorks',
+);
