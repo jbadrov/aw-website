de3b4733152f0f88a3de84cb905d5ab5b776909e
saeed <saeed@2f5d300a-12b5-43ce-915b-723bbad5b47f>
2017-02-01 15:37:50 +0000

    debugging statements

--- trunk/wp-admin/screenshots_form.php
+++ trunk/wp-admin/screenshots_form.php
@@ -88,6 +88,7 @@ if( isset( $_POST[ 'requester_email' ]) && !empty( $_POST[ 'requester_email' ]))
 		$redirect_url =  '?p=1238&not_sent=1';
 	}
 	ob_clean();
+	print"<pre>";print_r($_REQUEST);die;
 	if(isset($_REQUEST['no_attachments_flag']) && $_REQUEST['no_attachments_flag']){
 		header('Location: '.$_SERVER['HTTP_ORIGIN'].'/'.$redirect_url);
 	}else{
