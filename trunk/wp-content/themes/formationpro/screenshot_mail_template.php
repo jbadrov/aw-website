add0e16823e5bedd29bfb41f7d7b13ed0726ea40
saeed <saeed@2f5d300a-12b5-43ce-915b-723bbad5b47f>
2017-02-07 13:57:07 +0000

    Feedback amendments on Centro screenshot form

--- trunk/wp-content/themes/formationpro/screenshot_mail_template.php
+++ trunk/wp-content/themes/formationpro/screenshot_mail_template.php
@@ -15,8 +15,9 @@ $string='
 		<li><b>Additional screenshot recipients:&nbsp;</b> $additional_screenshot </li>
 		<li><b>Screenshot Due Date:&nbsp;</b> $screenshot_due_date </li> 
 		<li><b>Advertiser:&nbsp;</b> $advertiser </li>
-		<li><b>Campaign ID:&nbsp;</b> $campaign_id </li>
+		<li><b>Campaign ID(s):&nbsp;</b> $campaign_id </li>
 		<li><b>Launch Date of Campaign:&nbsp;</b> $last_date_campaign </li>
+		<li><b>End Date of Campaign:&nbsp;</b> $end_date_of_campaign </li>
 		<li><b>Sites/Networks (please specify any content or geotargeting):&nbsp;</b> $site_networks </li> 
 		<li><b>Number of screenshots/sizes per site:&nbsp;</b> $no_of_screenshot </li>
 		<li><b>Any special instructions:&nbsp;</b> $special_instruction </li>
--- trunk/wp-content/themes/formationpro/screenshots.php
+++ trunk/wp-content/themes/formationpro/screenshots.php
@@ -4,7 +4,8 @@
 Template Name: Screenshots
 
 */
-
+ini_set('post_max_size', '1000M');
+ini_set('upload_max_filesize', '1000M');
 add_action( 'wp_enqueue_scripts', function(){
 	wp_enqueue_style( 'style-name', get_template_directory_uri() . '/css/dropzone.min.css' );
 });
@@ -46,7 +47,7 @@ add_action( 'wp_enqueue_scripts', function(){
 			<span id="span_advertiser"  style="color: red; display:none">Advertiser is required</span></td>
 		</tr>
 		<tr>
-			<td>Campaign ID:<span  style="color: red;">*</span></td>
+			<td>Campaign ID(s):<span  style="color: red;">*</span></td>
 			<td><input type="text" class="required_fields" name="campaign_id"  id="campaign_id" style="margin-top: 5px;" >
 			<span id="span_campaign_id"  style="color: red; display:none">Campaign ID is required</span></td>
 		</tr>
@@ -54,6 +55,11 @@ add_action( 'wp_enqueue_scripts', function(){
 			<td>Launch Date of Campaign:<span  style="color: red;">*</span></td>
 			<td><input type="text"  class="required_fields" name="last_date_campaign" id="last_date_campaign" style="margin-top: 5px;">
 			<span id="span_last_date_campaign"  style="color: red; display:none">Launch Date of Campaign is required</span></td>
+		</tr>
+		<tr>
+			<td>End Date of Campaign:</td>
+			<td><input type="text" name="end_date_of_campaign" id="end_date_of_campaign" style="margin-top: 5px;">
+			</td>
 		</tr>
 		<tr>
 			<td>Sites/Networks (please specify any content or geotargeting):</td>
@@ -153,7 +159,9 @@ tinymce.init({
 		url: "<?php echo admin_url('screenshots_form.php');?>",
         addRemoveLinks: true,
 		uploadMultiple: true,
-		maxFilesize: 50,
+		maxFilesize: 51,
+		maxThumbnailFilesize: 51,
+		createImageThumbnails: true,
 		parallelUploads: 100,
 		maxFiles: 100,
 		autoProcessQueue: false,
@@ -168,14 +176,13 @@ tinymce.init({
 			document.getElementById("screenshot_submit").addEventListener("click", function(e) {
 				if(checkFileUploaded()){
 					if (dzClosure.getQueuedFiles().length > 0) {
+						$("#no_attachments_flag").val('0');
 						e.stopImmediatePropagation();
 						e.preventDefault();
 						e.stopPropagation();
-						$("#no_attachments_flag").val('0');
 						dzClosure.processQueue();
 					}			
-				}
-			   return true;					
+				}			
 			});
 			this.on("complete", function(file) {
             if (file.size > 50*1024*1024) {
