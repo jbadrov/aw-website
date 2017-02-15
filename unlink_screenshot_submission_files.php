32bb521b733c7ac6d47331254ac69c73e42b329e
saeed <saeed@2f5d300a-12b5-43ce-915b-723bbad5b47f>
2017-02-15 10:27:47 +0000

    Delete Old files before seven days

---
+++ unlink_screenshot_submission_files.php
@@ -0,0 +1,26 @@
+<?php
+require_once('wp-load.php');
+global $wpdb;
+$squantial_number = $wpdb->get_results("SELECT files_path FROM ".$wpdb->prefix."screenshot_form_submission");
+foreach($squantial_number as $file_path){
+	deleteOldFiles('wp-admin/'.$file_path->files_path);
+}
+function deleteOldFiles($targetfolder) {
+	$hours =168;
+	if (is_dir($targetfolder)){
+		$filelastmodified = filemtime($targetfolder);
+		if((time()-$filelastmodified) > $hours*3600)
+		{
+		  if ($dht = opendir($targetfolder)){
+			while (($file = readdir($dht)) !== false){
+				if($file=='..' || $file=='.'){
+					continue;
+				}
+					unlink($targetfolder.$file);
+			}
+			closedir($dht);
+		  }
+		   rmdir($targetfolder);
+		}				
+	}	
+}
