77564c06801f3957042f374ddd8d36535295734b
jbadrov <jbadrov@2f5d300a-12b5-43ce-915b-723bbad5b47f>
2017-02-16 05:14:47 +0000

    database changes

--- trunk/wp-admin/screenshots_form.php
+++ trunk/wp-admin/screenshots_form.php
@@ -7,7 +7,7 @@ require_once('../wp-load.php');
 global $wpdb;
 $squantial_number = $wpdb->get_var("SELECT max(ID) FROM ".$wpdb->prefix."screenshot_form_submission");
 if(empty($squantial_number)){
-	$squantial_number = 10001;
+	$squantial_number = 10000;
 }else{
 	$squantial_number =$squantial_number+1;
 }
@@ -19,7 +19,7 @@ error_reporting(0);
 ini_set('post_max_size', '1000M');
 ini_set('upload_max_filesize', '1000M');
 if( isset( $_POST[ 'requester_email' ]) && !empty( $_POST[ 'requester_email' ])) {
-	saveFormData($_POST,$targetfolder,$form_submission_id,$squantial_number);
+	saveFormData($_POST,$targetfolder,$form_submission_id);
 	$optional_name = basename($_POST['file_optional']);
 	$template_html= get_screenshot_mail_template();
 	$template_html = str_replace('form_submission_id', $form_submission_id, $template_html);
@@ -153,10 +153,10 @@ function create_zip($files = array(),$destination = '',$overwrite = false) {
 		return false;
 	}
 }
-function saveFormData($form_data,$files_path,$form_submission_id,$squantial_number){
+function saveFormData($form_data,$files_path,$form_submission_id){
 	global $wpdb;
-	$insert = "INSERT INTO ".$wpdb->prefix."screenshot_form_submission (ID,requester_email,additional_screenshot,screenshot_due_date,advertiser,campaign_id,last_date_campaign,end_date_of_campaign,site_networks,no_of_screenshot,file_optional,special_instruction,files_path,form_submission_id)
-				VALUES('{$squantial_number}','{$form_data['requester_email']}','{$form_data['additional_screenshot']}','{$form_data['screenshot_due_date']}','{$form_data['advertiser']}','{$form_data['campaign_id']}'
+	$insert = "INSERT INTO ".$wpdb->prefix."screenshot_form_submission (requester_email,additional_screenshot,screenshot_due_date,advertiser,campaign_id,last_date_campaign,end_date_of_campaign,site_networks,no_of_screenshot,file_optional,special_instruction,files_path,form_submission_id)
+				VALUES('{$form_data['requester_email']}','{$form_data['additional_screenshot']}','{$form_data['screenshot_due_date']}','{$form_data['advertiser']}','{$form_data['campaign_id']}'
 						,'{$form_data['last_date_campaign']}','{$form_data['end_date_of_campaign']}','{$form_data['site_networks']}','{$form_data['no_of_screenshot']}','{$form_data['file_optional']}','{$form_data['special_instruction']}','{$files_path}','{$form_submission_id}')";
 	$wpdb->query($insert);
 }
