06993d094ea83f4db1812fd62c0af532b6cf1c45
saeed <saeed@2f5d300a-12b5-43ce-915b-723bbad5b47f>
2017-02-22 08:55:40 +0000

    form submission redirect

--- wp-content/themes/formationpro/screenshot-form-submision.php
+++ wp-content/themes/formationpro/screenshot-form-submision.php
@@ -6,12 +6,12 @@ Template Name: Screenshot-FormSubmission
 */
  get_header('screenshot'); 
  if($_REQUEST['not_sent']==1){
-	 echo "<center><b><font color='red'>The email could not be sent to the email address entered. Please contact your administrator.</font></b>
-			<p><a href='/forms/clients/centro-screenshots/'>Click here to return</a></p></center>";
+	 echo '<center><b><font color="red">The email could not be sent to the email address entered. Please contact your administrator.</font></b>
+			<p><a href="'.site_url().'/forms/clients/centro-screenshots/">Click here to return</a></p></center>';
  }
  if($_REQUEST['form_submission_id']){
 	 echo '<center><b><font color="red">Form submission '.$_REQUEST['form_submission_id'].' successful</font></b>
-	 <p><a href="/forms/clients/centro-screenshots/">Click here to enter another request</a></p>
+	 <p><a href="'.site_url().'/forms/clients/centro-screenshots/">Click here to enter another request</a></p>
 	 </center>';
  }
  ?>
